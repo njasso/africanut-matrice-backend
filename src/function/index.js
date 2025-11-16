13307956aa4c6cddd8a9fbc7e6d5d84dc114c6df
njasso <efelixmagloire@gmail.com>
2025-11-16 18:53:57 +0100

    Update index.js

--- src/function/index.js
+++ src/function/index.js
@@ -1,1 +1,7 @@
+// backend/src/function/index.js
+
+module.exports = async function handler(req, res) {
+  console.log("Fonction exécutée !");
+  res.status(200).json({ message: "Fonction prête" });
+};
 
